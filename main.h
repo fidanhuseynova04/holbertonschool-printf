823edce3693a6aa671d56fd45b17dc479b60338a
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-30 19:33:07 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdlib.h>
+
+int print_string(unsigned int a, char *s);
+int print_char(unsigned int a, char c);
+int print_decimal(unsigned int a, int s);
+int _printf(const char *format, ...);
+#endif
